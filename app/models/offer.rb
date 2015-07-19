a919c6c4810ae8676e53731349e491f9675d4ba3
Cameron Kidman <cameronjkidman@gmail.com>
2015-07-18 20:34:06 -0600

    Add new offer sheets to offer array

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,5 +1,5 @@
 class Offer < ActiveRecord::Base
-  PROCESSORS = %w(Cardworks_Merrick Choice_Merrick DPS_Merchant eMerchant_Broker EMS_Merchant EMS_Harris EMS_Merrick Global_Merrick Humbolt_Harris LL_MERCHANT Merchant_eSolutions Meritus_Woodforest National_merchant_association_harris_bank NMC PaymentWorld_Esquire PaymentWorld_Merrick PowerPay Vantage_Humbolt VPS_Merchant Chesapeake_Merrick Gma_Harris Diet_Checklist Nma_Bmo_Mpa)
+  PROCESSORS = %w(Cardworks_Merrick Choice_Merrick DPS_Merchant eMerchant_Broker EMS_Merchant EMS_Harris EMS_Merrick Global_Merrick Humbolt_Harris LL_MERCHANT Merchant_eSolutions Meritus_Woodforest National_merchant_association_harris_bank NMC PaymentWorld_Esquire PaymentWorld_Merrick PowerPay Vantage_Humbolt VPS_Merchant Chesapeake_Merrick Gma_Harris Diet_Checklist Nma_Bmo_Mpa Paymitco_Merchant_Account_Ach_Icl Nma_Negative_Option)
   belongs_to :user
   has_many :processors
   after_create :create_processors
